e91f738cf625c181fceac2e5dfb402dcc0b768a8
Alexander <awsaavedra@gmail.com>
2016-03-08 18:18:06 -0800

    you sunk mah battle ship :(

---
+++ 1-learning-java/head-first-java-2/5-ch/SimpleDotCom.java
@@ -0,0 +1,29 @@
+public class SimpleDotCom{
+  
+  int [] locationCells;
+  int numOfHits = 0;
+
+  public void setLocationCells(int [] locs){
+    locationCells = locs;
+  }
+
+  public String checkYourself( String stringGuess){
+    int guess = Integer.parseInt(stringGuess); //convert string to int
+    String result = "miss";
+
+    for( int cell : locationCells){
+      if( guess == cell){
+        result = "hit";
+        numOfHits++;
+        break;
+      }
+    }
+
+    if( numOfHits == locationCells.length)
+      result = "kill";
+    
+    System.out.println(result);
+    
+    return result;
+  }
+}
