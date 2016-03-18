a5a253e5719e96997a65cfd478b8b0f2d5dc6885
Akul Aggarwal <akul.aggarwal@gmail.com>
2016-03-18 14:55:40 -0700

    akul finished lists exercise for ch4

--- 1-learning-language/eloquent/akul-eloquent/a-list.js
+++ 1-learning-language/eloquent/akul-eloquent/a-list.js
@@ -2,8 +2,6 @@ function arrayToList(arg){
   var list = {};
   var arg2 = list;
   
-
-
   function perEachOne(arg1) {
     arg1.value = arg[i];
     arg1.rest = {};
@@ -13,42 +11,67 @@ function arrayToList(arg){
     }
   } //end of perEachOne(arg1)
   
-
-
   for (i = 0; i < arg.length; i++) {
     perEachOne(arg2);
   }
-  
+  //console.log(list);
   return list;
-
-
 } //end of arrayToList(arg)
-
+///////////////////////////////
 
 function listToArray(arg){
   var flatArray = [];
   var theRest = arg;
-  
-  console.log(arg);
+  //console.log('original theRest: ', theRest);
+  //console.log(arrayToList(90,80,70);
 
   function perEachLayer(arg1){
+    //console.log(arg1.value);
+
     flatArray.push(arg1.value);
     theRest = arg1.rest;
-    
+    //console.log(flatArray);    
     if (theRest == null) {
       return flatArray;
     }
     else {
-      perEachLayer(theRest);
+      return perEachLayer(theRest);
     }
   } //end of perEachLayer()
 
   return perEachLayer(theRest);
-
-
 } //end of listToArray(arg)
+///////////////////////////////////////
+function prepend(element, list) {
+  var newList = {value: element, rest: list};
 
-console.log(arrayToList([10, 20,30]));
+  return newList
+} //end of prepend(element,list)
+/////////////////////////////////////
+function nth(list,number, i) {
+  var newList = list;
+  //console.log(i);
+  for (i; i < number; i++) {
+      //console.log(i);
+      newList = newList.rest;
+      i++;
+
+
+      if (newList === null) {
+        return undefined;
+      }
+
+      return nth(newList, number,i); 
+  }
+
+  return newList.value;
+} //end of nth(list,number)
+///////////////////////////////////////
+console.log(arrayToList([10, 20]));
 // → {value: 10, rest: {value: 20, rest: null}}
-console.log(listToArray(arrayToList[10,20,30]));
-
+console.log(listToArray(arrayToList([10, 20, 30])));
+// → [10, 20, 30]
+console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(nth(arrayToList([10, 20, 30]), 3, 0));
+// → 20
